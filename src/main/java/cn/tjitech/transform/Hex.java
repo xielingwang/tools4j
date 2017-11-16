ad0a38419bb1dbbb235a570255f9925ffe9ed763
AminBy <baiyaming@tjitech.cn>
2017-11-16 16:44:37 +0800

    [feature]Hex

---
+++ src/main/java/cn/tjitech/transform/Hex.java
@@ -0,0 +1,21 @@
+package cn.tjitech.transform;
+
+public class Hex {
+    private final static char[] HEX_CHAR = "0123456789ABCDEF".toCharArray();
+    public static String format(byte b) {
+        char[] chars = new char[2];
+        chars[0] = HEX_CHAR[(b & 0xFF) >>> 4];
+        chars[1] = HEX_CHAR[b & 0x0F];
+        return new String(chars);
+    }
+
+    public static String format(byte[] data) {
+        char[] chars = new char[data.length * 2];
+        for (int i = 0; i < data.length; i++) {
+            byte b = data[i];
+            chars[2 * i] = HEX_CHAR[(b & 0xFF) >>> 4];
+            chars[2 * i + 1] = HEX_CHAR[b & 0x0F];
+        }
+        return new String(chars);
+    }
+}
---
+++ src/test/java/cn/tjitech/transform/HexTest.java
@@ -0,0 +1,30 @@
+package cn.tjitech.transform;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class HexTest {
+
+    @Test
+    public void testFormatByte() {
+        byte[] bytes = new byte[] {
+                0x00,0x05,0x0A,0x0F,0x50,0x55,0x5A,0x5F,0x70,0x75,0x7A,0x7F,0x60,0x65,0x6A,0x6F, (byte) 0xFF
+        };
+        String[] hexStrs = new String[] {
+                "00","05","0A","0F","50","55","5A","5F","70","75","7A","7F","60","65","6A","6F","FF"
+        };
+        for (int i = 0; i < bytes.length; i ++) {
+            byte b = bytes[i];
+            assertEquals(hexStrs[i], Hex.format(b));
+        }
+    }
+
+    @Test
+    public void testFormatBytes() {
+        byte[] bytes = new byte[] {
+                0x00,0x05,0x0A,0x0F,0x50,0x55,0x5A,0x5F,0x70,0x75,0x7A,0x7F,0x60,0x65,0x6A,0x6F, (byte) 0xFF
+        };
+        String target = "00050A0F50555A5F70757A7F60656A6FFF";
+        assertEquals(target, Hex.format(bytes));
+    }
+}
